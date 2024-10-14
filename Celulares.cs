3b1d39f04b9d7f48d1f71d24edf662baacecd293
Oliveira04 <106097760+Oliveira04@users.noreply.github.com>
2024-10-14 14:29:38 -0300

    Add files via upload

---
+++ Celulares.cs
@@ -0,0 +1,28 @@
+﻿namespace Sistema_Celulares;
+
+public abstract class Smartphone
+ {
+     public string Numero { get; set; }
+     
+
+     public Smartphone(string numero)
+     {
+         Numero = numero;
+        
+     }
+
+     public void Ligar()
+     {
+         Console.WriteLine("Ligando...");
+     }
+
+     public void ReceberLigacao()
+     {
+         Console.WriteLine("Recebendo ligação...");
+     }
+
+     public abstract void InstalarAplicativo(string nomeApp);
+     public abstract void Receber_Ligacao();
+ }
+
+ 
